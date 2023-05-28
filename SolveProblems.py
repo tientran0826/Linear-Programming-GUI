10044202055d07c48e7860d509ab278995330eaf
Khoa <caohuynhanhkhoa2002@gmail.com>
2023-05-28 23:30:27 +0700

    add solve 2_phase_method

--- SolveProblems.py
+++ SolveProblems.py
@@ -3,26 +3,45 @@ from simplex_method.two_phase_simplex import two_phase_simplex
 from Normalization import make_standard_form
 
 def solve_problem(user_inputs):
-    problem = make_standard_form(user_inputs) 
-    problem['obj_func']*= -1
+    problem = make_standard_form(user_inputs)
     
-    check_min = 1
-    if user_inputs['objective'] == 'minimize':
-        check_min = -1
+    if (any(x < 0 for x in problem['constraints'])):
+        check_min = 1
+        if user_inputs['objective'] == 'max':
+            check_min = -1
+        # initialize object
+        simx = two_phase_simplex()
+        # get equations and standarize them
+        simx.get_equations(problem)
+        simx.standard_form()
 
-    use_blands_rule = False
+        # call two-phase simplex method
+        opt_point, opt_value = simx.two_phase()
 
-    if (any(x == 0 for x in problem['constraints'])):
-        use_blands_rule = True
-    # initialize solver
-    solver = SimplexSolver(obj_func=problem['obj_func'],
-                    coeffs=problem['coeffs'],
-                    constraints=problem['constraints'])
+        # print results
+        print ('\nOutput:\n')
+        print ('Optimal point is {}'.format(opt_point))
+        print ('Optimal value is {}'.format(opt_value*check_min))
+    else:
+        problem['obj_func']*= -1
+        
+        check_min = 1
+        if user_inputs['objective'] == 'minimize':
+            check_min = -1
 
-    pivot_arounds,steps,sol = solver.solve(use_blands_rule=use_blands_rule,
-                    print_tableau=True, 
-                    No_var = problem['No_var'],
-                    check_min = check_min, 
-                    var_none = problem['var_none'],
-                    list_Var_positive = problem['list_Var_positive'],)
-    return problem,sol,steps,pivot_arounds
+        use_blands_rule = False
+
+        if (any(x == 0 for x in problem['constraints'])):
+            use_blands_rule = True
+        # initialize solver
+        solver = SimplexSolver(obj_func=problem['obj_func'],
+                        coeffs=problem['coeffs'],
+                        constraints=problem['constraints'])
+
+        pivot_arounds,steps,sol = solver.solve(use_blands_rule=use_blands_rule,
+                        print_tableau=True, 
+                        No_var = problem['No_var'],
+                        check_min = check_min, 
+                        var_none = problem['var_none'],
+                        list_Var_positive = problem['list_Var_positive'],)
+        return problem,sol,steps,pivot_arounds
